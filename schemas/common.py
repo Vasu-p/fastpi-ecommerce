88756fe2af2d1cb1c98e2850b38f9b23e2f0b6d3
Vasudev Patel <vasudev.patel@sap.com>
2024-12-18 23:15:01 -0800

    create DocumentCreate base class for create requests

--- schemas/common.py
+++ schemas/common.py
@@ -1,10 +1,15 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
-from pyobjectID import PyObjectId
+from pyobjectID import PyObjectId, MongoObjectId
 
 
 class APIResponse(BaseModel):
     code: int
     message: str
+
+class DocumentCreate(BaseModel):
+    id: Optional[MongoObjectId] = Field(alias="_id", default=None)
 
 class DocumentUpdate(BaseModel):
     id: PyObjectId = Field(alias="_id")
--- schemas/user.py
+++ schemas/user.py
@@ -1,12 +1,10 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, Field
-from pyobjectID import MongoObjectId
-from schemas.common import DocumentUpdate
+from pydantic import EmailStr
+from schemas.common import DocumentUpdate, DocumentCreate
 
 
-class User(BaseModel):
-    id: Optional[MongoObjectId] = Field(alias="_id", default=None)
+class User(DocumentCreate):
     name: str
     email: EmailStr
 
