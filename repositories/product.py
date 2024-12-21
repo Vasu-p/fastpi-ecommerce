660a7fdbb4fbb4246bb61325d67c754cb73469e0
Vasudev Patel <vasudev.patel@sap.com>
2024-12-20 21:14:14 -0800

    implement filtering on product

--- repositories/product.py
+++ repositories/product.py
@@ -6,6 +6,26 @@ from repositories.util import PAGINATION_AGGREGATION, SORT_AGGREGATION
 from schemas.common import PaginationParams, SortParams, PaginatedData
 from schemas.product import ProductFilterParams
 
+def FILTER_AGGREGATION(filter_params: ProductFilterParams):
+    match_dict = {}
+    if filter_params.search:
+        match_dict["$text"] = { "$search": filter_params.search }
+    if filter_params.min_price:
+        if "$and" not in match_dict:
+            match_dict["$and"] = []
+        match_dict["$and"].append({ "price": { "$gt": filter_params.min_price } })
+    if filter_params.max_price:
+        if "$and" not in match_dict:
+            match_dict["$and"] = []
+        match_dict["$and"].append({ "price": { "$lt": filter_params.max_price } })
+    if filter_params.category:
+        match_dict["category"] = filter_params.category
+    if filter_params.brand:
+        match_dict["brand"] = filter_params.brand
+
+    return {
+        "$match": match_dict
+    }
 
 class ProductRepository(BaseRepository):
     def __init__(self):
@@ -16,9 +36,12 @@ class ProductRepository(BaseRepository):
                       filter_params: ProductFilterParams, db: AsyncIOMotorDatabase):
         try:
             results =  await db[self.collection].aggregate([
+                FILTER_AGGREGATION(filter_params),
                 SORT_AGGREGATION(sort_params),
                 PAGINATION_AGGREGATION(page_params)
             ]).next()
-            return PaginatedData(data=results["data"], total_count=results["page_meta"][0]["total_count"], **page_params.dict())
-        except:
+            total_count = results["page_meta"][0]["total_count"] if len(results["page_meta"]) > 0 else 0
+            return PaginatedData(data=results["data"], total_count=total_count, **page_params.dict())
+        except Exception as e:
+            print(e)
             return None
